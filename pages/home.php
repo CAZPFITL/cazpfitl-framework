3953efd8e5127a31cf643e05ff226ed7d8ef5186
Carlos M <carlos@bold.black>
2020-11-14 00:07:41 -0600

    instructions updated

--- pages/home.php
+++ pages/home.php
@@ -4,10 +4,12 @@
     <p>It has a system that allows to create dynamic html link tags (it uses the first class_</p>
     <p>used in the link tag to call the page with the same name)</p>
     <br>
+    <br>
     <h2 class="welcome">¡START!</h2>
     <p>To start creating pages, you must create a page on "pages" folder</p>
     <p>the next step it's adding the page name to the positions array!! (It must be the same name as the file).</p>
     <p>The positions array it's located on "index.php"</p>
+    <br>
     <br>
     <h2 class="welcome">NOW!</h3>
     <p>This is it! Now that page already exist!</p>
@@ -19,31 +21,49 @@
     <p>This only if the href attribute is not set, because if the href is set</p>
     <p>this url will have priority on the action.</p>
     <br>
-    <a class="link">link</a>
-    <a class="nofile">no file</a>
-    <a class="404">404</a>
-    <a href="https://www.google.com/" class="venta">External Link</a>
+    <blockquote>
+        <?php echo htmlspecialchars('<a class="link">link</a>') ?>
+        <a class="link">link</a>
+    </blockquote>
+    <br>
+    <blockquote>
+        <?php echo htmlspecialchars('<a class="nofile">no file</a>') ?>
+        <a class="nofile">no file</a>
+    </blockquote>
+    <br>
+    <blockquote>
+        <?php echo htmlspecialchars('<a class="404">404</a>') ?>
+        <a class="404">404</a>
+    </blockquote>
+    <br>
+    <blockquote>
+        <?php echo htmlspecialchars('<a href="https://www.google.com/" class="venta">External Link</a>') ?>
+        <a href="https://www.google.com/" class="venta">External Link</a>
+    </blockquote>
+    <br>
+    <br>
     <br>
     <h2 class="welcome">One last thing</h3>
     <p>You can create PHP objects if you want more order with those forms.</p>
     <p>You only have to call one this functions:</p>
     <br>
     <blockquote>
-        <p>< ?php</p>
+        <p><?php echo htmlspecialchars('<?php') ?></p>
             <blockquote>
-            <p>insertShared($target,$from)</p>
-            <p>insertPage($target)</p>
-            <p>insertObject($target)</p>
+            <p>insertShared($target,$from);</p>
+            <p>insertPage($target);</p>
+            <p>insertObject($target);</p>
             </blockquote>
-        <p>?></p>
+        <p><?php echo htmlspecialchars('?>') ?></p>
     </blockquote>
     <br>
     <p>-The target part, it's pretty obvious, but just in case, it's the name of the file you will add</p>
     <p>-The $from variable it's pretty obvious too, but just in case (too), it's the position where is</p>
     <p>called that object (from a page from a shared view, other object)</p>
     <br>
+    <br>
     <h2 class="welcome">Databases</h3>
-    <p>Or those database conections. mmm talking about database connections!!</p>
+    <p>Mmm talking about database connections!!</p>
     <p>You can add a database connection to the file</p>
     <p>"_bdconnection.static.php" located in 'assets/php/includes' folder</p>
     <span class="signature">-CAZPFITL</span>
