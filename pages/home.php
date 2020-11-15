99ddc5bd6d0c4cdac97b70e385274bdd3ae67a6d
Carlos M <carlos@bold.black>
2020-11-15 01:45:10 -0600

    feat: styling responsive

--- pages/home.php
+++ pages/home.php
@@ -1,53 +1,66 @@
-<div class="container">
+<style>
+    blockquote {
+        background: #f3f3f3;
+        padding: 20px;
+    }
+    .a {
+        color:#c70101;
+    }
+    .b {
+        color:#448244;
+    }
+    .c {
+        color:#c79d00;
+    }
+    .d,a {
+        color:#0088ff;
+    }
+    p{
+        line-height: 200%;
+    }
+</style>
+<div class="container" style="padding:40px;">
     <h1 class="welcome">¡WELCOME!</h1>
-    <p>This is my micro-framework, It allows you a fast deployment of pages using an array to declare.</p>
-    <p>It has a system that allows to create dynamic html link tags (it uses the first class_</p>
-    <p>used in the link tag to call the page with the same name)</p>
-    <br>
+    <p>This is my micro-framework, It allows you a fast deployment of pages using an array to declare.
+    It has a system that allows to create dynamic html link tags (it uses the first class 
+    used in the link tag to call the page with the same name)</p>
     <br>
     <h2 class="welcome">¡READY TO START!</h2>
-    <p>To start creating pages, you must create a page on "pages" folder.</p>
-    <p>The next step it's adding the page name to the positions array!! (It must be the same name as the file).</p>
-    <p>The positions array it's located on "index.php" This is it! Now that page already exist!</p>
+    <p>To start creating pages, you must create a page on "pages" folder.
+    The next step it's adding the page name to the positions array!! (It must be the same name as the file).
+    The positions array it's located on "index.php" This is it! Now that page already exist!</p>
+    <p>Don't Forget this: The first element in the array will be the home page.</p>
     <br>
-    <p>Don't Forget: The first element in the array will be the home page.</p>
-    <br>
-    <h2 class="welcome">¡LINKS!</h3>
-    <br>
-    <p>As you can see, this framework uses #hashes to set the locations</p>
-    <p>So for links you have to add a link tag <?php echo htmlspecialchars(' <a> ') ?>. The first class setted will be read by the framework</p>
-    <p>and will automatically update all those locations in the page at load process:</p> 
+    <h2 class="welcome">¡LINKS!</h2>
+    <p>As you can see, this framework uses #hashes to set the locations.
+    So for links you have to add a link tag <?php echo htmlspecialchars(' <a> ') ?>. The first class setted will be read by the framework
+    and will automatically update all those locations in the page at load process:</p> 
     <br>
     <div style="display:flex;flex-direction:column;align-items:center;">
-        <img src="assets/images/1.png" alt="1">
+        <img src="assets/images/1.png" alt="1" style="max-width:50%;">
         <p>▼</p>
-        <img src="assets/images/2.png" alt="2">
+        <img src="assets/images/2.png" alt="2" style="max-width:100%;">
     </div>
     <br>
-    <p>This only if the href attribute is not set, because if the href is set</p>
-    <p>this url will have priority on the action.</p>
+    <p>This only if the href attribute is not set, because if the href is set
+    this url will have priority on the action.</p>
     <br>
     <blockquote>
-        <?php echo htmlspecialchars('<a class="link">link</a>') ?>
+        <span><</span><span class="a">a</span><span> </span><span class="b">class</span><span>=</span><span class="c">"link"</span><span>>link<</span>/<span class="a">a</span>>
         <a class="link">link</a>
-
     </blockquote>
-    <br>
     <blockquote>
-        <?php echo htmlspecialchars('<a class="nofile">no file</a>') ?>
+        <span><</span><span class="a">a</span><span> </span><span class="b">class</span><span>=</span><span class="c">"nofile"</span><span>>no file<</span>/<span class="a">a</span>>
         <a class="nofile">no file</a>
     </blockquote>
-    <br>
     <blockquote>
-        <?php echo htmlspecialchars('<a class="404">404</a>') ?>
+        <span><</span><span class="a">a</span><span> </span><span class="b">class</span><span>=</span><span class="c">"404"</span><span>>404<</span>/<span class="a">a</span>>
         <a class="404">404</a>
     </blockquote>
-    <br>
     <blockquote>
-        <?php echo htmlspecialchars('<a href="https://www.google.com/" class="venta">External Link</a>') ?>
+        <span><</span><span class="a">a</span><span> </span><span class="b">href</span><span>=</span><span class="d">"https://www.google.com/" </span><span class="b">class</span><span>=</span><span class="c">"link"</span><span>>External Link<</span>/<span class="a">a</span><span>></span>
         <a href="https://www.google.com/" class="venta">External Link</a>
     </blockquote>
-    <br>
     <br>
     <br>
     <h2 class="welcome">One last thing</h3>
--- shared/header.dontmissthis.php
+++ shared/header.dontmissthis.php
@@ -13,7 +13,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=0.7, user-scalable=no">
 
 		<!-- title -->
-		<title>EMEDE</title>
+		<title>CMONROY</title>
 
 		<!-- Page Styles -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
